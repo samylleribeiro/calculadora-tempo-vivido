62fee7cb3098fc70ab234dabd495e7c6dfc8685a
Samylle Carvalho <samylliribeiro2018@gmail.com>
2026-05-25 19:32:48 -0300

    ajustes css para responsividade no celular

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 function tempoVivido() {
+    // pega os valores que o usuário digitou
     let dataInput = document.getElementById("dataNascimento").value;
     let resultado = document.getElementById("resultado");
-
+    // verifica se foi informado a data de nascimento
     if (!dataInput) {
         resultado.innerHTML = "Por favor, insira uma data de nascimento válida ❌.";
-        document.body.style.backgroundColor = "#ffebee";
-        resultado.style.color = "#c62828";
-        return;
+     if (document.body.classList.contains("dark-mode")) {
+        document.body.style.backgroundColor = "#2d1a1a"; // Fundo vinho escuro
+        resultado.style.color = "#ff8a80"; // 🌟 Vermelho bem clarinho/pastel (perfeito para ler no escuro!)
+    } else {
+        document.body.style.backgroundColor = "#ffebee"; // Fundo rosa claro
+        resultado.style.color = "#c62828"; // Vermelho escuro
+    } 
+    
+      document.querySelector('.container-resultado').style.display = 'block';
+      document.getElementById('dataNascimento').focus(); // direciona para adicionar a data de nascimento
+       return;
     }
+    // volta para o estilo padrão quando não estiver erro
+    document.body.style.backgroundColor = ""; 
+    resultado.style.color = "";
 
     let dataNascimento = new Date(dataInput + "T00:00:00");
     let dataAtual = new Date();
-
+    // verifica se a data de nascimento não e maior que a data atual
     if (isNaN(dataNascimento) || dataNascimento > dataAtual) {
         resultado.innerHTML = "Por favor, insira uma data de nascimento válida ❌.";
-        document.body.style.backgroundColor = "#ffebee"; 
-        resultado.style.color = "#c62828"; 
+        if (document.body.classList.contains("dark-mode")) {
+        document.body.style.backgroundColor = "#2d1a1a"; // Fundo vinho escuro
+        resultado.style.color = "#ff8a80"; // 🌟 Vermelho bem clarinho/pastel (perfeito para ler no escuro!)
+    } else {
+        document.body.style.backgroundColor = "#ffebee"; // Fundo rosa claro
+        resultado.style.color = "#c62828"; // Vermelho escuro
+    } 
+    
+      document.querySelector('.container-resultado').style.display = 'block';
+      document.getElementById('dataNascimento').focus(); // direciona para adicionar a data de nascimento
         return;
     }
+    // volta para o estilo padrão quando não estiver erro
+    document.body.style.backgroundColor = ""; 
+    resultado.style.color = "";
+
 
     let diferenca = dataAtual - dataNascimento;
 
@@ -28,12 +52,14 @@ function tempoVivido() {
     let minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
     let segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
 
-    // Deixando a exibição bem limpa e organizada
+    // Exibindo o resultado na tela
     resultado.innerHTML = `🎉 Você já viveu:<br>
     <strong>${anos}</strong> anos, <strong>${meses}</strong> meses, <strong>${dias}</strong> dias,<br>
     <strong>${horas}</strong> horas, <strong>${minutos}</strong> minutos e <strong>${segundos}</strong> segundos!`;   
     document.body.style.backgroundColor = "";
-    resultado.style.color = "";   
+    resultado.style.color = "";  
+    // Caixinha branca so quando tiver resultado
+    document.querySelector('.container-resultado').style.display = 'block'; 
     confetti();
 }
 
@@ -43,7 +69,18 @@ function limpar() {
     resultado.innerHTML = "";
     resultado.style.color = ""; 
     document.body.style.backgroundColor = "";
+    document.getElementById('dataNascimento').focus();         // direciona para adicionar a data de nascimento
+    document.querySelector('.container-resultado').style.display = 'none';    // esconde caixinha branca
 }
 function toggleDarkMode() {
+    // lida e desliga o modo noturno
     document.body.classList.toggle("dark-mode");
+      // 2. Pega o botão lá no HTML pelo ID dele
+    let botao = document.getElementById("btn-noturno");   
+    // 3. Verifica se o body ESTÁ com o modo noturno ativado
+    if (document.body.classList.contains("dark-mode")) {
+        botao.innerText = "☀️"; //  o botão vira Sol
+    } else {
+        botao.innerText = "🌙"; // o botão vira Lua
+    }
 }
